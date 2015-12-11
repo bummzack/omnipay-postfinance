6e0ce35b468aec8fa8b81b8fae8018cabc1b51a7
Roman Schmid <bummzack@gmail.com>
2015-12-11 16:40:30 +0100

    Added test to ensure correctness of the SHA hashing.

--- tests/GatewayTest.php
+++ tests/GatewayTest.php
@@ -35,6 +35,57 @@ class GatewayTest extends GatewayTestCase
     }
 
 
+    public function testSHASign()
+    {
+        $data = array(
+            'PSPID' => 'MyPSPID',
+            'ORDERID' => '1234',
+            'AMOUNT' => 1500,
+            'CURRENCY' => 'EUR',
+            'LANGUAGE' => 'en_US'
+        );
+
+        // reconstruct the hashes from the official documentation and ensure we calculate the same hashes
+        $this->assertEquals(
+            'F4CC376CD7A834D997B91598FA747825A238BE0A',
+            Helper::create_sha_hash($data, 'Mysecretsig1875!?', 'sha1'),
+            'SHA-1 hash does not match the excepted output'
+        );
+
+        $this->assertEquals(
+            'E019359BAA3456AE5A986B6AABD22CF1B3E09438739E97F17A7F61DF5A11B30F',
+            Helper::create_sha_hash($data, 'Mysecretsig1875!?', 'sha256'),
+            'SHA-256 hash does not match the excepted output'
+        );
+
+        $this->assertEquals(
+            'D1CFE8833A297D0922E908B2B44934B09EE966EF1584DC0D696304E07BB58BA71973C2383C831D878D8A243BB7D7DFFFBE53CEE21955CDFEF44FE82E551F859D',
+            Helper::create_sha_hash($data, 'Mysecretsig1875!?', 'sha512'),
+            'SHA-512 hash does not match the excepted output'
+        );
+
+        // reconstruct sha-out example from the official documentation
+        $outData = array(
+            'BRAND' => 'VISA',
+            'ACCEPTANCE' => 1234,
+            'amount' => 15,
+            'CARDNO' => 'XXXXXXXXXXXX1111',
+            'NCERROR' => 0,
+            'currency' => 'EUR',
+            'PAYID' => 32100123,
+            'PM' => 'CreditCard',
+            'orderID' => 12,
+            'STATUS' => 9
+        );
+
+        $this->assertEquals(
+            '209113288F93A9AB8E474EA78D899AFDBB874355',
+            Helper::create_sha_hash($outData, 'Mysecretsig1875!?', 'sha1'),
+            'SHA-OUT hash does not match the excepted output (sha1)'
+        );
+    }
+
+
     public function testCompletePurchaseSuccess()
     {
         $data = array(
